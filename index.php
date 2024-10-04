31141c5bea01fd02cf56c4bcadfb3fb83e5e11ef
Dilusha Madushan <164498659+dilushamadushan@users.noreply.github.com>
2024-10-05 00:08:03 +0530

    created home section 1

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php include("header.php") ?>
+<head>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<div class="section_1">   
+        <div id="con">
+           <h1 class="fw-bolder">Welcome To Our Library </h1>
+            <p class="fw-medium">Here, you can explore a wide range of books, e-books, and resources. 
+               Use the search bar to find what you're looking for, check out featured books, or
+               browse different categories. If you're a member, log in to see your account details and manage your loans. 
+               Not a member yet? Sign up easily to start borrowing today !</p>
+
+            <div class="d-grid gap-2 d-md-block home-btn">
+                <button type="button" class="btn btn-outline-light btn-lg btn_find">Find Book</button>
+                <button type="button" class="btn btn-outline-light btn-lg">See More</button>
+            </div>
+        </div>
+</div>
+<script src="assets/js/index.js"></script>
+
 <?php include("footer.php") ?>

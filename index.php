b8d0aec6797cf113c974741968dc305e982cf4cd
Dilusha Madushan <164498659+dilushamadushan@users.noreply.github.com>
2024-10-17 16:37:52 +0530

    added quikly access section

--- index.php
+++ index.php
@@ -15,5 +15,50 @@
         </div>
 </div>
 
+<div class="quick-access-section">
+    <h2 class="fw-bold">Quickly Access Page</h2>
+    <p>Welcome to the Most Popular Library Today</p>
+    <div class="decorative-line-wrapper">
+        <div class="decorative-line"></div>
+        <i class="fa-solid fa-book"></i>
+        <div class="decorative-line"></div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-md-3 mb-4">
+            <div class="content-card">
+                <img src="assets/media/home-main/book.jpg" alt="Book Image" class="img-fluid">
+                <h4>BOOK</h4>
+                <p>Explore our collection of physical and digital books across various genres.</p>
+                <button class="read-more-btn">Read More</button>
+            </div>
+        </div>
+        <div class="col-md-3 mb-4">
+            <div class="content-card">
+                <img src="assets/media/home-main/maga.jfif" alt="Magazine Image" class="img-fluid">
+                <h4>MAG & ARTICLE</h4>
+                <p>Stay updated with the latest articles and magazines on diverse topics. </p>
+                <button class="read-more-btn">Read More</button>
+            </div>
+        </div>
+        <div class="col-md-3 mb-4">
+            <div class="content-card">
+                <img src="assets/media/home-main/author.png" alt="Journal Image" class="img-fluid">
+                <h4>AUTHOR</h4>
+                <p>Discover works from both emerging and renowned authors in our library.</p>
+                <button class="read-more-btn">Read More</button>
+            </div>
+        </div>
+        <div class="col-md-3 mb-4">
+            <div class="content-card">
+                <img src="assets/media/home-main/news.png" alt="Newspaper Image" class="img-fluid">
+                <h4>NEWSPAPER</h4>
+                <p>Read the latest news from leading newspapers around the world.</p>
+                <button class="read-more-btn">Read More</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php include("footer.php") ?>

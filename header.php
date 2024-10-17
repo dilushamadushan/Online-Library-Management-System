0dd944d893e46a4947f4784fba0149db8b89616c
Dilusha Madushan <164498659+dilushamadushan@users.noreply.github.com>
2024-10-17 11:07:11 +0530

    added font awesome cdn

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap css link -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Font Awesome CDN-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <!-- Header css link -->
     <link rel="stylesheet" href="assets/css/header.css">
     <!-- Footer css link -->

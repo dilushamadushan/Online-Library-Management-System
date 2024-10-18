3a785a780d49c72928649b9bb5bf0559feb4cd86
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2024-10-18 21:08:08 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,63 @@
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/jquery-3.7.1.js"></script>
+<footer class="text-white mt-auto py-4">
+    <div class="container">
+        <div class="row justify-content-evenly">
+            <div class="col-md-3 contact-info">
+                <h5>Library</h5>
+                <div class="line-foo"></div>
+                <ul class="list-unstyled">
+                    <li><i class="fa-solid fa-phone"></i> Phone</li>
+                    <li class="ms-5">General: 071999999</li>
+                    <li class="ms-5">Librarian: 09877</li>
+                    <li><i class="fa-solid fa-fax"></i> Fax: +94 412227028</li>
+                    <li><i class="fa-solid fa-envelope"></i> E-mail: librarian@lib.ruh.ac.lk</li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 quick-links">
+                <h5>Quick Links</h5>
+                <div class="line-foo"></div>
+                <ul class="list-unstyled">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Author</a></li>
+                    <li><a href="#">News And Event</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 catalog">
+                <h5>Catalog</h5>
+                <div class="line-foo"></div>
+                <ul class="list-unstyled">
+                    <li><i class="fa-solid fa-book"></i> <a href="#">Book</a></li>
+                    <li><i class="fa-solid fa-database"></i> <a href="#">E-Resources</a></li>
+                    <li><i class="fa-solid fa-newspaper"></i> <a href="#">Newspaper</a></li>
+                    <li><i class="fa-solid fa-file"></i> <a href="#">Magazine & Article</a></li>
+                    <li><i class="fa-solid fa-paperclip"></i> <a href="#">Past Paper</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 text-center">
+                <div class="visit-counter">
+                    <div class="line-foo"></div>
+                    <div class="px-2 fw-bolder">Visit Counter</div>
+                    <div class="line-foo"></div>
+                </div>
+                <p><i class="fa-solid fa-chart-simple"></i> <span class="ms-2">Total Visit</span></p>
+                <div class="total-count">00</div>
+                <p><i class="fa-solid fa-chart-area"></i> <span class="ms-2">Today Visit</span></p>
+                <div class="today-count">00</div>
+                <i class="fa-solid fa-hands-holding-circle icon-center"></i>
+            </div>
+        </div>
+        <hr class="border-light">
+        <div class="text-center">
+            <p>&copy; 2024 Public Library. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/jquery-3.7.1.js"></script>
+<script src="../assets/js/index.js"></script>
 </body>
 </html>

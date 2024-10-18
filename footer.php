4a43a7c92cb975609bccdba14da4e86d979b56d6
HajithMohamed <165193756+HajithMohamed@users.noreply.github.com>
2024-10-18 20:56:48 +0530

    footer

--- footer.php
+++ footer.php
@@ -56,8 +56,8 @@
         </div>
     </div>
 </footer>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/jquery-3.7.1.js"></script>
-<script src="assets/js/index.js"></script>
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/jquery-3.7.1.js"></script>
+<script src="../assets/js/index.js"></script>
 </body>
 </html>

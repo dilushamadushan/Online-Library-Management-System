ca4fd6054791c13fe6991df26910347c20abcd33
Dilusha Madushan <164498659+dilushamadushan@users.noreply.github.com>
2024-10-26 00:27:26 +0530

    update page

--- user-account.php
+++ user-account.php
@@ -53,8 +53,206 @@
         </div>
 
         <div class="main">
-
-
+            <div id="profile" class="main-section active">
+                <div class="profile-details">
+                    <div class="main-topic">
+                            <h1>Personal information</h1>
+                    </div>
+                    <div class="user-info-main">
+                        <div class="user-img">
+                            <img src="assets/media/admin-page/avatar.png" alt="User Image">
+                        </div>
+                        <div class="user-info">
+                                <div class="user-info-details">
+                                    <span>Name</span>
+                                    <p>Dilusha madushan</p>
+                                </div>
+                                <div class="user-info-details">
+                                    <span>Email</span>
+                                    <p>dilushamadushan@gmail.com</p>
+                                </div>
+                                <div class="user-info-details">
+                                    <span>Contact Number</span>
+                                    <p>202-2223332</p>
+                                </div>
+                                <div class="user-info-details">
+                                    <span>Address</span>
+                                    <p>No 2, Abc, Kandy</p>
+                                </div>
+                                <button id="user-update"><i class="fa-solid fa-pen"></i>Edit Profile</button>
+                        </div>
+                    </div>
+                    <div class="update-form">
+                        <form action="" method="POST">
+                            <label>Name</label><br>
+                            <input type="text" name="up-name"><br>
+                            <label>Email</label><br>
+                            <input type="email" name="up-email" ><br>
+                            <label>Contact Number</label><br>
+                            <input type="tel" name="up-email" ><br>
+                            <label>Address</label><br>
+                            <input type="text" name="up-address" ><br>
+                            <button id="update-user-btn" class="user-prof-btn" value="Update">Update</button>
+                            <button id="profBack-user" class="user-prof-btn" value="Back">Back</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div id="books" class="main-section">
+                <div class="main-topic">
+                    <h1>Books</h1>
+                </div>
+                <div id="book-btn">
+                    <button id="list-book" class="book-main-btn"><i class="fa-solid fa-list"></i> Book List</button>
+                    <button id="return-book" class="book-main-btn"><i class="fa-solid fa-rotate-left"></i> Book Return</button>
+                    <button id="issued-book" class="book-main-btn"><i class="fa-regular fa-bookmark"></i> Book Issued</button>
+                </div>
+                <div id="book-res" class="book-res">
+                    <div id="list-book-sec" class="section-table">
+                        <table class="book-table">
+                            <tr>
+                                <th>Book Number</th>
+                                <th>Book Name</th>
+                                <th>ISBN Number</th>
+                                <th>Author</th>
+                                <th>Action</th>
+                            </tr>
+                            <tr>
+                                <td>100</td>
+                                <td>Harry Potter</td>
+                                <td>85-223</td>
+                                <td>J. K. Rowling</td>
+                                <td>Return</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div id="return-book-sec">
+                        <div id="list-book-sec" class="section-table">
+                            <table class="book-table">
+                                <tr>
+                                    <th>book id</th>
+                                    <th>Book Name</th>
+                                    <th>Get Date</th>
+                                    <th>Returned Date</th>
+                                    <th>Action</th>
+                                </tr>
+                                <tr>
+                                    <td>501</td>
+                                    <td>Becoming</td>
+                                    <td>12-12-1291</td>
+                                    <td>15-08-2023</td>
+                                    <td>Return</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div id="issued-book-sec">
+                        <div id="list-book-sec" class="section-table">
+                            <table class="book-table">
+                                <tr>
+                                    <th>Book ID</th>
+                                    <th>Book Name</th>
+                                    <th>Issued Date</th>
+                                    <th>Returned Date</th>
+                                    <th>Action</th>
+                                </tr>
+                                <tr>
+                                    <td>1001</td>
+                                    <td>Harry Potter</td>
+                                    <td>12-10-2023</td>
+                                    <td>01-11-2023</td>
+                                    <td>Return</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="E-Resourses" class="main-section">
+                    <div class="main-topic">
+                        <h1>Books</h1>
+                    </div>
+                    <div id="book-btn">
+                        <button id="e-list-book" class="book-main-btn"><i class="fa-solid fa-list"></i> Book List</button>
+                        <button id="e-return-book" class="book-main-btn"><i class="fa-solid fa-rotate-left"></i> Book Return</button>
+                        <button id="e-issued-book" class="book-main-btn"><i class="fa-regular fa-bookmark"></i> Book Issued</button>
+                    </div>
+                    <div id="e-book-res" class="e-book-res">
+                        <div id="e-list-book-sec" class="e-section-table">
+                            <table class="e-book-table">
+                                <tr>
+                                    <th>Book Number</th>
+                                    <th>Book Name</th>
+                                    <th>ISBN Number</th>
+                                    <th>Author</th>
+                                    <th>Action</th>
+                                </tr>
+                                <tr>
+                                    <td>100</td>
+                                    <td>Harry Potter</td>
+                                    <td>85-223</td>
+                                    <td>J. K. Rowling</td>
+                                    <td>Return</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div id="e-return-book-sec">
+                            <div id="e-list-book-sec" class="e-section-table">
+                                <table class="e-book-table">
+                                    <tr>
+                                        <th>book id</th>
+                                        <th>Book Name</th>
+                                        <th>Get Date</th>
+                                        <th>Returned Date</th>
+                                        <th>Action</th>
+                                    </tr>
+                                    <tr>
+                                        <td>501</td>
+                                        <td>Becoming</td>
+                                        <td>12-12-1291</td>
+                                        <td>15-08-2023</td>
+                                        <td>Return</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                        <div id="e-issued-book-sec">
+                            <div id="e-list-book-sec" class="e-section-table">
+                                <table class="e-book-table">
+                                    <tr>
+                                        <th>Book ID</th>
+                                        <th>Book Name</th>
+                                        <th>Issued Date</th>
+                                        <th>Returned Date</th>
+                                        <th>Action</th>
+                                    </tr>
+                                    <tr>
+                                        <td>1001</td>
+                                        <td>Harry Potter</td>
+                                        <td>12-10-2023</td>
+                                        <td>01-11-2023</td>
+                                        <td>Return</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <div id="update-password" class="main-section">
+                <div class="main-topic">
+                    <h1>Change Password</h1>
+                </div>
+                <div class="pass-change-form">
+                <form action="" method="">
+                    <h3>Current Password :</h3>
+                    <input type="password" class="form-clz" name="up-cPass">
+                    <h3>New Password :</h3>
+                    <input type="password" class="form-clz"  name="up-nPass">
+                    <h3>Re-type Password :</h3>
+                    <input type="password" class="form-clz"  name="up-rPass"><br>
+                    <input type="button" name="pass-btn" value="Change Password">
+                </form>
+            </div>
         </div>
     </div>
 </div>
